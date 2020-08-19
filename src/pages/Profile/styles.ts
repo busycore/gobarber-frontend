a58991205784c8e07885f3940a41789dcb36c784
Matheus V <matheusvrgm@gmail.com>
2020-08-18 23:27:27 -0300

    Fix use history warn

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import { shade } from 'polished';
 //import SignupBackground from '../../assets/sign-up-background.png';
 
